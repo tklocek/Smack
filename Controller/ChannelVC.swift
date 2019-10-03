1be2147bcca9175780fd874dec67cc1a9764aa75
Tomasz Klocek <tomek@klocek.info>
2019-10-03 18:04:19 +0200

    End of lesson 02

---
+++ Controller/ChannelVC.swift
@@ -0,0 +1,32 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Tomek Klocek on 2019-10-03.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controller/ChatVC.swift
@@ -0,0 +1,38 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Tomek Klocek on 2019-10-03.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlet
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

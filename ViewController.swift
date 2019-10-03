b248bc85afadb56cb4b09f0fc6550440254ea02e
Tomasz Klocek <tomek@klocek.info>
2019-10-03 11:05:50 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Tomek Klocek on 2019-10-03.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

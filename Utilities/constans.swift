eec2a7e617d44be6f9e936801ba95bb944e36af9
Tomasz Klocek <tomek@klocek.info>
2019-10-04 17:56:30 +0200

    Lesson 09

--- Utilities/constans.swift
+++ Utilities/constans.swift
@@ -8,8 +8,25 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://tom-chatty.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+
 // Seques
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedin"
+let USER_EMAIL = "userEmail"
+
+

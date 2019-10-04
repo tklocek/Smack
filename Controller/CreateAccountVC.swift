ea50d184e2da73d89e6ce708359d1c5c516dd8c5
Tomasz Klocek <tomek@klocek.info>
2019-10-04 21:11:47 +0200

    End of Lesson 10

--- Controller/CreateAccountVC.swift
+++ Controller/CreateAccountVC.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passTxt.text , passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (sucess) in
+            if sucess {
+                print("registered user")
+            }
+        }
     }
     
     @IBAction func closePressed(_ sender: Any) {
@@ -21,4 +47,7 @@ class CreateAccountVC: UIViewController {
     }
     
 
+    
+    
+    
 }
